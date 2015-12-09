3bb8c0d9011d0ad05c4676db826a1fb23d173a86
YuLei <yulei@rcnp.osaka-u.ac.jp>
2015-12-10 06:24:09 +0900

    no error for dst2root.C and fast

--- dst2root.C
+++ dst2root.C
@@ -8,25 +8,29 @@
 #include "TTree.h"
 #include "TROOT.h"
 #include "TMath.h"
+#include "TFile.h"
 #include "TBenchmark.h"
+#include "TRandom.h"
 #include "constant.h"
+
 
 using namespace std;
 
-void dst2root(TString openFileName){ //the file name should be "../dstroot/runXXXX_asci.dst"
+void dst2root(TString openFileName, Int_t nEntries = 990000000){ //the file name should be "../dstroot/runXXXX_asci.dst"
+   printf("=============================\n");
    gROOT->ProcessLine(".!date");
-   gStyle->SetOptStat(0);
    
    TString saveFileName = openFileName;
-   printf("%s \n", saveFileName.Data());
+   printf("input .... %s \n", saveFileName.Data());
    saveFileName.Remove(0,11); //remove head
    Int_t len = saveFileName.Length();
    saveFileName.Remove(len  - 9); // remove tail
    saveFileName = saveFileName + ".root";
-   printf("%s \n", saveFileName.Data());
+   printf("output ... %s , nEntries:%d\n", saveFileName.Data(), nEntries);
    
    TFile *f1 = new TFile (saveFileName,"recreate");
    TTree * t1 = new TTree("tree","tree");
+   TRandom * rand = new TRandom();
    //================ Tree branch
    //---------eventID
    Int_t eventID = 0;
@@ -129,21 +133,21 @@ void dst2root(TString openFileName){ //the file name should be "../dstroot/runXX
    Int_t gradc[4],grtdc[4],grrf,adc[12],adc2[14],tdc[12],tdc2[12],lrf[3],dummy;
    
    TBenchmark clock;
-   Bool_t shown;
+   Bool_t shown = 0;
    
    clock.Reset();
    clock.Start("timer");
    shown = 0;
    
-   do{
+   Int_t pos1, pos2;
    
-      
-      //if( eventID > 10) break;
+   do{
       
       eventID ++;
+      if( eventID > nEntries) break;
       
+      //------------ CoinReg
       fp>>coinReg;
-      
       //----------- GR
       fp>>grx;
       fp>>gry;
@@ -154,14 +158,14 @@ void dst2root(TString openFileName){ //the file name should be "../dstroot/runXX
       fp>>gradc[1];
       fp>>gradc[2];
       fp>>gradc[3];
-      fp>>grtdc[0];
-      fp>>grtdc[1];
-      fp>>grtdc[2];
-      fp>>grtdc[3];
+      fp>>grtdc[0]; if(grtdc[0] == -1) grtdc[0] = TMath::QuietNaN();
+      fp>>grtdc[1]; if(grtdc[1] == -1) grtdc[1] = TMath::QuietNaN();  
+      fp>>grtdc[2]; if(grtdc[2] == -1) grtdc[2] = TMath::QuietNaN();  
+      fp>>grtdc[3]; if(grtdc[3] == -1) grtdc[3] = TMath::QuietNaN();  
       //------------ GR rf
       fp>>grrf;
       //------------ Stack ADC
-      fp>>adc[0];
+      fp>>adc[0]; 
       fp>>adc[1];
       fp>>adc[2];
       fp>>adc[3];
@@ -189,37 +193,38 @@ void dst2root(TString openFileName){ //the file name should be "../dstroot/runXX
       fp>>adc2[12];
       fp>>adc2[13];
       //------------ STACK TDC
-      fp>>tdc[0];
-      fp>>tdc[1];
-      fp>>tdc[2];
-      fp>>tdc[3];
-      fp>>tdc[4];
-      fp>>tdc[5];
-      fp>>tdc[6];
-      fp>>tdc[7];
-      fp>>tdc[8];
-      fp>>tdc[9];
-      fp>>tdc[10];
-      fp>>tdc[11];
+      fp>>tdc[0];  if(tdc[0]  == -1) tdc[0] = TMath::QuietNaN();
+      fp>>tdc[1];  if(tdc[1]  == -1) tdc[1] = TMath::QuietNaN();
+      fp>>tdc[2];  if(tdc[2]  == -1) tdc[2] = TMath::QuietNaN();
+      fp>>tdc[3];  if(tdc[3]  == -1) tdc[3] = TMath::QuietNaN();
+      fp>>tdc[4];  if(tdc[4]  == -1) tdc[4] = TMath::QuietNaN();
+      fp>>tdc[5];  if(tdc[5]  == -1) tdc[5] = TMath::QuietNaN();
+      fp>>tdc[6];  if(tdc[6]  == -1) tdc[6] = TMath::QuietNaN();
+      fp>>tdc[7];  if(tdc[7]  == -1) tdc[7] = TMath::QuietNaN();
+      fp>>tdc[8];  if(tdc[8]  == -1) tdc[8] = TMath::QuietNaN();
+      fp>>tdc[9];  if(tdc[9]  == -1) tdc[9] = TMath::QuietNaN();
+      fp>>tdc[10]; if(tdc[10] == -1) tdc[10] = TMath::QuietNaN();
+      fp>>tdc[11]; if(tdc[11] == -1) tdc[11] = TMath::QuietNaN();
       //------------- BAND telesope, Liquid TDC
-      fp>>tdc2[0];
-      fp>>tdc2[1];
-      fp>>tdc2[2];
-      fp>>tdc2[3];
-      fp>>tdc2[4];
-      fp>>tdc2[5];
-      fp>>tdc2[6];
-      fp>>tdc2[7];
-      fp>>tdc2[8];
-      fp>>tdc2[9];
-      fp>>tdc2[10];
-      fp>>tdc2[11];
+      fp>>tdc2[0]; if(tdc2[0]  == -1) tdc2[0] = TMath::QuietNaN(); 
+      fp>>tdc2[1]; if(tdc2[1]  == -1) tdc2[1] = TMath::QuietNaN(); 
+      fp>>tdc2[2]; if(tdc2[2]  == -1) tdc2[2] = TMath::QuietNaN(); 
+      fp>>tdc2[3]; if(tdc2[3]  == -1) tdc2[3] = TMath::QuietNaN(); 
+      fp>>tdc2[4]; if(tdc2[4]  == -1) tdc2[4] = TMath::QuietNaN(); 
+      fp>>tdc2[5]; if(tdc2[5]  == -1) tdc2[5] = TMath::QuietNaN(); 
+      fp>>tdc2[6]; if(tdc2[6]  == -1) tdc2[6] = TMath::QuietNaN(); 
+      fp>>tdc2[7]; if(tdc2[7]  == -1) tdc2[7] = TMath::QuietNaN(); 
+      fp>>tdc2[8]; if(tdc2[8]  == -1) tdc2[8] = TMath::QuietNaN(); 
+      fp>>tdc2[9]; if(tdc2[9]  == -1) tdc2[9] = TMath::QuietNaN(); 
+      fp>>tdc2[10];if(tdc2[10] == -1) tdc2[10] = TMath::QuietNaN();
+      fp>>tdc2[11];if(tdc2[11] == -1) tdc2[11] = TMath::QuietNaN();
       //------------- Liquid 
       fp>>lrf[0];
       fp>>lrf[1];
       fp>>lrf[2];
       //fp>>dummy;
-      
+      // fp>>coinReg>>grx>>grth>>gry>>grph>>gradc[0]>>gradc[1]>>gradc[2]>>gradc[3]>>grtdc[0]>>grtdc[1]>>grtdc[2]>>grtdc[3]>>grrf>>adc[0]>>adc[1]>>adc[2]>>adc[3]>>adc[4]>>adc[5]>>adc[6]>>adc[7]>>adc[8]>>adc[9]>>adc[10]>>adc[11]>>adc2[0]>>adc2[1]>>adc2[2]>>adc2[3]>>adc2[4]>>adc2[5]>>adc2[6]>>adc2[7]>>adc2[8]>>adc2[9]>>adc2[10]>>adc2[11]>>adc2[12]>>adc2[13]>>tdc[0]>>tdc[1]>>tdc[2]>>tdc[3]>>tdc[4]>>tdc[5]>>tdc[6]>>tdc[7]>>tdc[8]>>tdc[9]>>tdc[10]>>tdc[11]>>tdc2[0]>>tdc2[1]>>tdc2[2]>>tdc2[3]>>tdc2[4]>>tdc2[5]>>tdc2[6]>>tdc2[7]>>tdc2[8]>>tdc2[9]>>tdc2[10]>>tdc2[11]>>lrf[0]>>lrf[1]>>lrf[2]>>dummy;
+
       //================================ 2ndary data processing
       //------------- Axuillary 
       grXC = grx - 380./2.3*grth*TMath::RadToDeg();
@@ -230,8 +235,8 @@ void dst2root(TString openFileName){ //the file name should be "../dstroot/runXX
       if((tdc2[8]<0)&&tdc2[9]<0) vetogate += 1;
       
       //------------- RF
-      grf = (grrf + gRandom->Uniform(0,1))*GR_CH2NS[0];
-      brf = (lrf[1] + gRandom->Uniform(0,1))*LAS_CH2NS[0];
+      grf = (grrf + rand->Uniform(0,1))*GR_CH2NS[0];
+      brf = (lrf[1] + rand->Uniform(0,1))*LAS_CH2NS[0];
       //------------ GR 
       grdE1 = TMath::Sqrt(gradc[0]*gradc[1]);
       grdE2 = TMath::Sqrt(gradc[2]*gradc[3]);
@@ -239,8 +244,8 @@ void dst2root(TString openFileName){ //the file name should be "../dstroot/runXX
       grT1avg = (grtdc[0]+36+grtdc[1])/2.*GR_CH2NS[0];
       grT2avg = (grtdc[2]+36+grtdc[3])/2.*GR_CH2NS[0];
   
-      grTOF1 = grT1avg + (gRandom->Uniform(0,1)+gRandom->Uniform(0,1))/2.*GR_CH2NS[0] - grf + GR_TOF1_OFFSET;
-      grTOF2 = grT2avg + (gRandom->Uniform(0,1)+gRandom->Uniform(0,1))/2.*GR_CH2NS[0] - grf + GR_TOF1_OFFSET;
+      grTOF1 = grT1avg + (rand->Uniform(0,1)+rand->Uniform(0,1))/2.*GR_CH2NS[0] - grf + GR_TOF1_OFFSET;
+      grTOF2 = grT2avg + (rand->Uniform(0,1)+rand->Uniform(0,1))/2.*GR_CH2NS[0] - grf + GR_TOF1_OFFSET;
       
       //----------- STACK 
       sta1h = sqrt(adc[0]/112.*adc[1]/118.);
@@ -264,19 +269,19 @@ void dst2root(TString openFileName){ //the file name should be "../dstroot/runXX
       sta3vTavg = (tdc[8] + tdc[9] )/2.*LAS_CH2NS[8];
       sta4vTavg = (tdc[10]+ tdc[11])/2.*LAS_CH2NS[9];
       
-      if(sta1hTavg != -LAS_CH2NS[4] ) sta1hTOF = sta1hTavg + ((gRandom->Uniform(0,1)+gRandom->Uniform(0,1))/2. - (lrf[1] + gRandom->Uniform(0,1)))*LAS_CH2NS[4] + STACK_TOF_OFFSET[0];
-      if(sta2hTavg < 700*LAS_CH2NS[5] ) sta2hTOF = sta2hTavg + ((gRandom->Uniform(0,1)+gRandom->Uniform(0,1))/2. - (lrf[1] + gRandom->Uniform(0,1)))*LAS_CH2NS[5] + STACK_TOF_OFFSET[1];
-      if(sta1vTavg != -LAS_CH2NS[6] ) sta1vTOF = sta1vTavg + ((gRandom->Uniform(0,1)+gRandom->Uniform(0,1))/2. - (lrf[1] + gRandom->Uniform(0,1)))*LAS_CH2NS[6] + STACK_TOF_OFFSET[2];
-      if(sta2vTavg != -LAS_CH2NS[7] ) sta2vTOF = sta2vTavg + ((gRandom->Uniform(0,1)+gRandom->Uniform(0,1))/2. - (lrf[1] + gRandom->Uniform(0,1)))*LAS_CH2NS[7] + STACK_TOF_OFFSET[3];
-      if(sta3vTavg != -LAS_CH2NS[8] ) sta3vTOF = sta3vTavg + ((gRandom->Uniform(0,1)+gRandom->Uniform(0,1))/2. - (lrf[1] + gRandom->Uniform(0,1)))*LAS_CH2NS[8] + STACK_TOF_OFFSET[4];
-      if(sta4vTavg != -LAS_CH2NS[9] ) sta4vTOF = sta4vTavg + ((gRandom->Uniform(0,1)+gRandom->Uniform(0,1))/2. - (lrf[1] + gRandom->Uniform(0,1)))*LAS_CH2NS[9] + STACK_TOF_OFFSET[5];
+      sta1hTOF = sta1hTavg + ((rand->Uniform(0,1)+rand->Uniform(0,1))/2. - (lrf[1] + rand->Uniform(0,1)))*LAS_CH2NS[4] + STACK_TOF_OFFSET[0];
+      sta2hTOF = sta2hTavg + ((rand->Uniform(0,1)+rand->Uniform(0,1))/2. - (lrf[1] + rand->Uniform(0,1)))*LAS_CH2NS[5] + STACK_TOF_OFFSET[1];
+      sta1vTOF = sta1vTavg + ((rand->Uniform(0,1)+rand->Uniform(0,1))/2. - (lrf[1] + rand->Uniform(0,1)))*LAS_CH2NS[6] + STACK_TOF_OFFSET[2];
+      sta2vTOF = sta2vTavg + ((rand->Uniform(0,1)+rand->Uniform(0,1))/2. - (lrf[1] + rand->Uniform(0,1)))*LAS_CH2NS[7] + STACK_TOF_OFFSET[3];
+      sta3vTOF = sta3vTavg + ((rand->Uniform(0,1)+rand->Uniform(0,1))/2. - (lrf[1] + rand->Uniform(0,1)))*LAS_CH2NS[8] + STACK_TOF_OFFSET[4];
+      sta4vTOF = sta4vTavg + ((rand->Uniform(0,1)+rand->Uniform(0,1))/2. - (lrf[1] + rand->Uniform(0,1)))*LAS_CH2NS[9] + STACK_TOF_OFFSET[5];
    
            
       //------------- Telescope      
       badEl = adc2[8];
       badEr = adc2[9];
       blo1 = TMath::Sqrt(adc2[0]*adc[1]);
-      blo2 = TMAth::Sqrt(adc2[2]*adc[3]);
+      blo2 = TMath::Sqrt(adc2[2]*adc[3]);
       blo3 = TMath::Sqrt(adc2[4]*adc[5]);
       blo4 = TMath::Sqrt(adc2[6]*adc[7]);
       
@@ -290,21 +295,20 @@ void dst2root(TString openFileName){ //the file name should be "../dstroot/runXX
       blo3TOF = TMath::QuietNaN();
       blo4TOF = TMath::QuietNaN();
    
-      if( tdc2[8] != -1 ) badElTOF = (tdc2[8] + gRandom->Uniform(0,1))*LAS_CH2NS[0] - brf ;
-      if( tdc2[9] != -1 ) badErTOF = (tdc2[9] + gRandom->Uniform(0,1))*LAS_CH2NS[0] - brf ;
-      if(blo1Tavg != -LAS_CH2NS[0] )blo1TOF = blo1Tavg + ((gRandom->Uniform(0,1)+gRandom->Uniform(0,1))/2. - (lrf[1] + gRandom->Uniform(0,1)))*LAS_CH2NS[0] + BLOCK_TOF_OFFSET[0];
-      if(blo2Tavg != -LAS_CH2NS[1] )blo2TOF = blo2Tavg + ((gRandom->Uniform(0,1)+gRandom->Uniform(0,1))/2. - (lrf[1] + gRandom->Uniform(0,1)))*LAS_CH2NS[1] + BLOCK_TOF_OFFSET[1];
-      if(blo3Tavg != -LAS_CH2NS[2] )blo3TOF = blo3Tavg + ((gRandom->Uniform(0,1)+gRandom->Uniform(0,1))/2. - (lrf[1] + gRandom->Uniform(0,1)))*LAS_CH2NS[2] + BLOCK_TOF_OFFSET[2];
-      if(blo4Tavg != -LAS_CH2NS[3] )blo4TOF = blo4Tavg + ((gRandom->Uniform(0,1)+gRandom->Uniform(0,1))/2. - (lrf[1] + gRandom->Uniform(0,1)))*LAS_CH2NS[3] + BLOCK_TOF_OFFSET[3];
-   
+      badElTOF = (tdc2[8] + rand->Uniform(0,1))*LAS_CH2NS[0] - brf ;
+      badErTOF = (tdc2[9] + rand->Uniform(0,1))*LAS_CH2NS[0] - brf ;
+      blo1TOF = blo1Tavg + ((rand->Uniform(0,1)+rand->Uniform(0,1))/2. - (lrf[1] + rand->Uniform(0,1)))*LAS_CH2NS[0] + BLOCK_TOF_OFFSET[0];
+      blo2TOF = blo2Tavg + ((rand->Uniform(0,1)+rand->Uniform(0,1))/2. - (lrf[1] + rand->Uniform(0,1)))*LAS_CH2NS[1] + BLOCK_TOF_OFFSET[1];
+      blo3TOF = blo3Tavg + ((rand->Uniform(0,1)+rand->Uniform(0,1))/2. - (lrf[1] + rand->Uniform(0,1)))*LAS_CH2NS[2] + BLOCK_TOF_OFFSET[2];
+      blo4TOF = blo4Tavg + ((rand->Uniform(0,1)+rand->Uniform(0,1))/2. - (lrf[1] + rand->Uniform(0,1)))*LAS_CH2NS[3] + BLOCK_TOF_OFFSET[3];
    
       //-------------- Liquid
       liqlf = adc2[10];
       liqld = adc2[11];
       liqrf = adc2[12];
       liqrd = adc2[13];
-      liqlTOF = (tdc2[10] + gRandom->Uniform(0,1))*LAS_CH2NS[0] - brf ;
-      liqrTOF = (tdc2[11] + gRandom->Uniform(0,1))*LAS_CH2NS[0] - brf ;
+      liqlTOF = (tdc2[10] + rand->Uniform(0,1))*LAS_CH2NS[0] - brf ;
+      liqrTOF = (tdc2[11] + rand->Uniform(0,1))*LAS_CH2NS[0] - brf ;
       
       //----------- Fill       
       f1->cd(); //set focus on this file
@@ -317,12 +321,11 @@ void dst2root(TString openFileName){ //the file name should be "../dstroot/runXX
 
       if ( !shown ) {
          if (fmod(time, 10) < 1 ){
-            printf( "%10d[%2d%%]|%3d min %5.2f sec | expect:%5.1fmin %10d\n", 
-               entry, 
-               TMath::Nint((entry+1)*100./totnumEntry),
-               TMath::Floor(time/60), time - TMath::Floor(time/60)*60,
-               totnumEntry*time/(entry+1)/60.,
-               count);
+            printf( "%10d[%5.2f%%]|%f|%3d min %5.2f sec\n", 
+               eventID,
+               eventID*100./nEntries,
+                    time,
+               TMath::Floor(time/60), time - TMath::Floor(time/60)*60);
                shown = 1;
          }
       }else{
@@ -335,18 +338,14 @@ void dst2root(TString openFileName){ //the file name should be "../dstroot/runXX
 //nextLine:
 //      getline(fp, line, '\r'); // read fp and store in line, and next line
 
-
+/**/
    }while(! fp.eof());
-   
+
+   f1->cd(); //set focus on this file
+   t1->Write(); 
+   f1->Close();
   
-  printf("=======================================\n");
-   
-   
-   /*//======================================================================
-   TFile *f1 = new TFile ("test.root","read");
-   TTree *t1 = (TTree*)f1->Get("t1");
-   
-   */
-   
+   printf("=======================================\n");
+   printf("%d \n", eventID);
    printf("............ done!\n");
 }
